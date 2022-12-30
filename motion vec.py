16428de2b8dc76e6d9d1e1b8efab2c59483eb789
JFFelizardo <j.francisco.felizardo@tecnico.ulisboa.pt>
2022-12-30 22:45:23 +0000

    wi

--- motion vec.py
+++ motion vec.py
@@ -1,8 +1,7 @@
-import numpy as np
 import serial
 import time
 import math
-from target_points_v2 import image_processing
+from target_points_final import image_processing
 
 
 def dist(point1, point2):
@@ -13,20 +12,26 @@ def dist(point1, point2):
 
 class scorbot:
     def __init__ (self):
+        """Init function of the class, checks the serial connection and initializes the class variables"""
         if serial:
             self.com = serial.Serial("COM3", baudrate=9600, bytesize=8, timeout=2, parity="N", xonxoff=0)
-        self.ipos=[0,0,0,0]
+        self.ipos=[0,0,0]
         pass
     
     def send(self, msg):
+        """Takes as argument a string and sends it 
+           in the proper format to the serial manipulator"""
         self.com.write(bytes(msg,'UTF-8'))
         pass
 
     def initpos(self,x,y,z,r):
+        """Changes the reference position coordinates
+           to have the home position coordinates,
+           which is a centimetre above the paper's
+           reference position"""
         self.ipos[0]=x
         self.ipos[1]=y
         self.ipos[2]=z + 100
-        self.ipos[3]=r
         pass
     
     def read_and_wait(self, wait_time):
@@ -55,19 +60,29 @@ class scorbot:
         return output
 
     def movestr(self, coordi, coordf):
+        """Function used in the first approach, repeatedly called for 
+           all the target points in the image"""
+
+        # sets the coordinates of the next point, always relative to the reference position
         self.send("SETPVC 1 X " + str(self.ipos[0]+int(coordf[0])) + "\r")
         time.sleep(0.5)
         self.send("SETPVC 1 Y " + str(self.ipos[1]+int(coordf[1])) + "\r")
         time.sleep(0.5)
         self.send("SETPVC 1 Z " + str(self.ipos[2]-100) + "\r")
         time.sleep(0.5)
-        # rf=roll_calc(self,coordi,coordf)
-        alfa = 1.2/1000
-        move_time = round(100*(0.3 + alfa*dist((coordi[0],coordi[1]),(coordf[0],coordf[1]))))
+
+        # Calculation of the time of the movement based on a fixed value of 0.3 seconds
+        # plus a factor (alpha) 2 seconds per 10 cm that is multiplied by the distance in the movement
+        alpha = 2/1000
+        move_time = round(100*(0.3 + alpha*dist((coordi[0],coordi[1]),(coordf[0],coordf[1]))))
+
+        # Sets the movement for the calculated time
         self.send("MOVE 1 " + str(move_time) + "\r")
         time.sleep(move_time/100)
 
     def movehome(self):
+        """Saves the current position to have the correct pitch and pen orientation.
+           Afterward moves to the home position 1 centimetre above the paper"""
         self.send("HERE 1 " + "\r")
         time.sleep(0.5)        
         self.send("SETPVC 1 X " + str(self.ipos[0]) + "\r")
@@ -80,10 +95,14 @@ class scorbot:
         time.sleep(0.6)
         self.send("HERE 1 " + "\r")
         time.sleep(0.5)
+
+        # sets the reference z position to the paper level
         self.send("SETPVC 1 Z " + str(self.ipos[2]-100) + "\r")
         time.sleep(0.5)
     
     def moveup(self,coord):
+        """Moves vertically two centimetres relative two the
+           reference position z coordinate"""
         self.send("SETPVC 1 X " + str(coord[0]) + "\r")
         time.sleep(0.5)
         self.send("SETPVC 1 Y " + str(coord[1]) + "\r")
@@ -94,6 +113,11 @@ class scorbot:
         time.sleep(0.6)
 
     def create_vector(self,N):
+        """Receives the number of points of the desired vector and
+           defines the name of the vector, sending a command for
+           it's creation to the serial manipulator and 
+           returning the name of the vector"""
+
         vect_name = "POSY"
         self.send("DIMP " + vect_name + " [" + str(N) + "]" + "\r")
         time.sleep(1)
@@ -101,20 +125,23 @@ class scorbot:
         return vect_name
     
     def add_vector(self,coord,n,vect_name):
+        """Adds the point to the given vector on the given position"""
+
+        # Copies the reference position and changes the x and y
         self.send("SETP " + vect_name + "[" + str(n) + "]=1" + "\r")
         time.sleep(0.5)
         self.read_and_wait(0.5)
         self.send("SETPVC " + vect_name + "[" + str(n) + "] X " + str(self.ipos[0]+int(coord[0])) + "\r")
         time.sleep(0.5)
-        # self.read_and_wait(0.5)
         self.send("SETPVC " + vect_name + "[" + str(n) + "] Y " + str(self.ipos[1]+int(coord[1])) + "\r")
         time.sleep(0.5)
-        # self.read_and_wait(0.5)
 
 
 def start(self):
-    """Start the robot, activating the axis and setting the speed to 50%
+    """Start the robot, resetting both the input and output buffers,
+       activating the axes and setting the speed to 1%
        The first position is beforehand manually selected"""
+    
     self.com.flushInput()
     time.sleep(1)
     self.com.flushOutput()
@@ -124,47 +151,103 @@ def start(self):
     self.send("SPEED 1 " + "\r")
 
 def homepos(self):
-    # self.com.flush()
+    """Recording of the home position, the reference position
+       for the drawing that is beforehand defined.
+       It sends the LISTPV POSITION command and
+       analises the ouput, sending it as arguments
+       to the initpos function"""
+
+    # read from the buffer what there is to read
     msg = self.com.inWaiting()
     msg = self.com.read(msg)
-    msg = str(msg)
+
+    # send the command and wait for the full output
     self.send("LISTPV POSITION " + "\r")
     time.sleep(1)
+
+    # reads the full output of the command
     msg = self.com.inWaiting()
     msg = self.com.read(msg)
     msg = str(msg)
+
+    # gets only the last line of the output
+    # containing the cartesian coordinates of the position
     msg = msg.split("X:")
     msg = msg[1]
+
+    # by splitting again and having a positive x and a negative y
+    # the following expressions can retrieve the coordinates
     msg = msg.split()
     x = int(msg[0])
     y = msg[1]
     y = int(y[2:])
     z = msg[3]
     z = int(z)
-    r = msg[5]
-    r = int(r[2:])
-    self.initpos(x,y,z,r)
+
+    # gets the coordinates to the class variables
+    self.initpos(x,y,z)
+
+def drawstr(self,pts):
+    """Draw function for the first approach, repeatedly calling the
+       movestr function for each point"""
+    for i in range(len(pts)):
+        if i==0:
+            self.movestr(self,self.ipos,pts[i])
+            self.read_and_wait(self,0.5)
+        self.movestr(self,pts[i-1],pts[i])
+    self.moveup(pts[i])
 
 def draw(self,pts):
+    """Takes the bot and the target points, creates the vector
+       of points in the serial manipulator and instructs the
+       movement through the points, ending on a vertical movement"""
+
+    # initialize the vector with the required number of positions
     cnt = len(pts)
     vect_name = self.create_vector(cnt)
     self.read_and_wait(1)
+
+    # for each set of coordinates, add them in the correct position
+    # in the vector, looping through all the points
     for i in range(len(pts)+1):
-        if i == 0:
+        # The position indexes on the created vector start at one
+        # so the position 1 of the vector corresponds to pts[0]
+        if i == 0: 
             pass
         else: self.add_vector(pts[i-1],i,vect_name)
     time.sleep(0.5)
+
+    # Calculates the movement time having 1.5 seconds per point
     move_time = cnt*150
+
+    # Commands the serial manipulator to go through all the positions
+    # in the defined movement time
     self.send("MOVES " + vect_name + " " + str(1) + " " + str(cnt) + " " + str(move_time) +"\r")
     self.read_and_wait(0.5)
+    time.sleep(move_time/100)
 
+    # reaching the final point, moves up to separate the pen from the paper
     self.moveup(pts[cnt-1])
 
+
+# initializes the object
 bot=scorbot()
+
+# starts the serial manipulator
 start(bot)
+
+# registers the reference position
 homepos(bot)
+
+# moves to the home position
 bot.movehome()
 
+# Executes the image processing
 pts = image_processing("test_draw_2.png")
+
+"""To define the name of the vector go to the create_vector function"""
+# Executes the drawing
 draw(bot,pts)
+
+# After the drawing, returns to the home position
 bot.movehome()
--- target_points_final.py
+++ target_points_final.py
@@ -6,7 +6,11 @@ import imutils
 
 # This function returns the target points
 def image_processing(image):
-    
+    """This function takes an image as input, finds
+    its contours and calculates its
+    target points. The return is a 
+    vector of points with coordinates (x,y)"""
+   
     # load the image and display it
     image = cv2.imread(image)
 
@@ -18,22 +22,24 @@ def image_processing(image):
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     cnts = imutils.grab_contours(cnts)
 
-    # draw the contours finded and save it to a png file
+    # draw the contours finded
     img_contours = np.zeros(image.shape)
     cv2.drawContours(img_contours, cnts, -1, (0,255,0), 1)
-    #cv2.imwrite('contornos.png',img_contours)
+    
+    # saves the image of the draw containing the contours finded (uncomment the next line)
+    # cv2.imwrite('contornos.png',img_contours)
     
     
     # if the image is just composed by one contour:
     if len(cnts) == 1:
         
-        # get the draw with the final contour approximation and save it to a png file
+        # get the draw with the final contour approximation
         for c in cnts:
             epsilon_aux = 0.0003 * cv2.arcLength(c, True)
             approx_aux = cv2.approxPolyDP(c, epsilon_aux,True)
             cv2.drawContours(img_contours, [approx_aux], -1, (0,255,0),3)
-            text = "eps={:.4f}, num_pts={}".format(0.0003, len(approx_aux))
-
+           
+            # saves the image of the draw containing the contours approximation (uncomment the next line)
             # cv2.imwrite('contornos_approx.png'.format(c), img_contours)
 
         
@@ -58,7 +64,7 @@ def image_processing(image):
         # convert target point list into a numpy array 
         target_points_list = np.asarray(target_points_list)
         
-        # plot x,y target points to pixels
+        """1) Plot the target points in pixels """ # uncomment until plt.show()
         # y, x = target_points_list.T
         # plt.scatter(x,y)
         # plt.xlim(0,image.shape[0])
@@ -66,7 +72,7 @@ def image_processing(image):
         # plt.title("Target points in pixels")
         # plt.show()
 
-        # plot x,y target points to centimeters (cm)
+        """2) Plot the target points in centimeters """ # uncomment until plt.show()
         # y, x = target_points_list.T
         # y = y*0.0264583333
         # x = x*0.0264583333
@@ -76,7 +82,7 @@ def image_processing(image):
         # plt.title("Target points in centimeters (cm)")
         # plt.show()
 
-        # x,y target points according to a 16x16 cm square
+        """Transform the draw according to a 16x16 square"""
         y, x = target_points_list.T
         maximum = max(image.shape[0], image.shape[1])
         k = 1600/maximum
@@ -101,22 +107,22 @@ def image_processing(image):
         # get the draw with the single vector containing both contours
         img_contours = np.zeros(image.shape)
         cv2.drawContours(img_contours, points, -1, (0,255,0), 4)
-
-        # To get the image of the single vector containing both contours uncomment the following line
-        # cv2.imwrite('contornos_case2.png', img_contours)
+        
+        # saves the image of the draw containing the single vector defined above (uncomment the next line)
+        # cv2.imwrite('contornos_case2.png', img_contours) 
         
         # convert all the points list into a numpy array
         points = np.asarray(points)
         
-        # get the final target points approximation vector and save it to a png file
+        # get the final target points approximation vector
         k = 0.0003
         peri = cv2.arcLength(points, True)
         approx = cv2.approxPolyDP(points,k*peri, True)
-        
+      
         # define the target points vector
         target_points_list = approx 
         
-        # plot x,y target points to pixels
+        """1) Plot the target points in pixels """ # uncomment until plt.show()
         # y, x = target_points_list.T
         # plt.scatter(x,y)
         # plt.xlim(0,image.shape[0])
@@ -124,7 +130,7 @@ def image_processing(image):
         # plt.title("Target points in pixels")
         # plt.show()
 
-        # plot x,y target points to centimeters (cm)
+        """2) Plot the target points in centimeters """ # uncomment until plt.show()
         # y, x = target_points_list.T
         # y = y*0.0264583333
         # x = x*0.0264583333
@@ -134,7 +140,7 @@ def image_processing(image):
         # plt.title("Target points in centimeters (cm)")
         # plt.show()
 
-        # x,y target points according to a 15x15 cm square
+        """Transform the draw according to a 15x15 square"""
         y, x = target_points_list.T
         maximum = max(image.shape[0], image.shape[1])
         k = 1500/maximum
@@ -149,5 +155,4 @@ def image_processing(image):
         return pts
         
 
-pts = image_processing("test_draw_1.png")
-#print(pts)
+# pts = image_processing("test_draw_2.png")
