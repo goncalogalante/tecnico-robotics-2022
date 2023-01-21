06fab0045693027ab0946724ebc7443e8e74ef11
JFFelizardo <j.francisco.felizardo@tecnico.ulisboa.pt>
2023-01-21 16:30:16 +0000

    Update coordinates_to_y.py

--- coordinates_to_y.py
+++ coordinates_to_y.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from matplotlib import pyplot as plt
 
 # estimativa de distância percorrida em cada percurso
 # estimativa da posição em cada ponto de cada percurso consoante as medidas
@@ -37,15 +38,29 @@ def load_vect(name,n,max):
 
     return vect
 
-basename = "./Lab2./Dados./"
-curva = load_vect(basename + "curva_e_",10,36)
-pf_curva = load_vect(basename + "curva_e_pf_",5,21)
-reta = load_vect(basename + "reta_e_",8,57)
-pf_reta = load_vect(basename + "reta_e_pf_",5,17)
-pi_reta = load_vect(basename + "reta_e_pi_",4,15)
-pf_pass = load_vect(basename + "reta_p_pf_",5,20)
+
+def get_data():
+    basename = "./Lab2./Dados./"
+    a = load_vect(basename + "curva_e_",10,36)
+    b = load_vect(basename + "curva_e_pf_",5,21)
+    c = load_vect(basename + "reta_e_",8,57)
+    d = load_vect(basename + "reta_e_pf_",5,17)
+    e = load_vect(basename + "reta_e_pi_",4,15)
+    f = load_vect(basename + "reta_p_pf_",5,20)
+
+    return [a,b,c,d,e,f]
+
+
+def get_points(vect):
+    ()
+
+
+[curva,pf_curva,reta,pf_reta,pi_reta,pf_pass] = get_data()
+
 
 true_pi_reta = [38.73799549745399, -9.138472338872601, 81]
 true_pf_reta = [38.73795591274537, -9.138953366340852, 81]
 true_pf_curva = [38.73790047032445, -9.13911362832949, 81]
 true_pf_pass = [38.73755264660175, -9.139080771269057, 81]
+
+print(pi_reta[0])
